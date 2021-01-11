f4d7ed1ab8e695c47c4183e29fa4b9ecddc086f8
developerVincentAmbrosio <vince.ambrosio@gamil.com>
2021-01-11 18:19:19 -0500

    worked on module

--- sql-scripts/create.shopping-list.sql
+++ sql-scripts/create.shopping-list.sql
@@ -6,7 +6,7 @@ CREATE TYPE grocery AS ENUM (
     'Breakfast'
 );
 
-CREATE TABLE IF NOT EXISTS shopping_list )
+CREATE TABLE IF NOT EXISTS shopping_list (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     price decimal(10, 2) NOT NULL,
